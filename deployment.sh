dd4cc902a8a9b1ee80549215991c5fd7af3551b1
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-03-27 09:17:21 +0400

    Initial commit - basically a fork of aegir_cid

---
+++ deployment.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Wrapper script for our fabfile, to be called from Jenkins
+#
+
+# Where our fabfile is
+FABFILE=/usr/local/bin/aegir_cid/fabfile.py
+
+HOST=$1
+SITE=$2
+PROFILE=$3
+WEBSERVER=$4
+DBSERVER=$5
+MAKEFILE=$6
+BUILD_NUMBER=$7
+DATE=`date +%Y%m%d%H%M%S`
+
+if [[ -z $HOST ]] || [[ -z $SITE ]] || [[ -z $PROFILE ]] || [[ -z $WEBSERVER ]] || [[ -z $DBSERVER ]] || [[ -z $MAKEFILE ]] || [[ -z $BUILD_NUMBER ]]
+then
+  echo "Missing args! Exiting"
+  exit 1
+fi
+
+
+# Array of tasks - these are actually functions in the fabfile, as an array here for the sake of abstraction
+if [ $BUILD_NUMBER -eq "1" ]; then
+  # This is a first-time ever build. Let's install a site instead of migrate it
+  TASKS=(
+   build_platform
+   save_alias
+   install_site
+  )
+else
+  TASKS=(
+    build_platform
+    migrate_site
+    save_alias
+    import_site
+  )
+fi
+
+# Loop over each 'task' and call it as a function via the fabfile, 
+# with some extra arguments which are sent to this shell script by Jenkins
+for task in ${TASKS[@]}; do
+  fab -f $FABFILE -H $HOST $task:site=$SITE,profile=$PROFILE,webserver=$WEBSERVER,dbserver=$DBSERVER,makefile=$MAKEFILE,build=$DATE || exit 1
+done
---
+++ fabfile.py
@@ -0,0 +1,41 @@
+from fabric.api import *
+import time
+
+env.user = 'aegir'
+env.shell = '/bin/bash -c'
+
+# Download and import a platform using Drush Make
+def build_platform(site, profile, webserver, dbserver, makefile, build):
+  print "===> Building the platform..."
+  run("drush make %s /var/aegir/platforms/%s" % (makefile, build))
+  run("drush --root=/var/aegir/platforms/%s provision-save @platform_%s --web_server=@server_%s --context_type=platform --debug" % (build, build, webserver))
+  run("drush @hostmaster hosting-import '@platform_%s'" % build)
+  run("drush @hostmaster hosting-dispatch")
+
+# Install a site on a platform, and kick off an import of the site
+def install_site(site, profile, webserver, dbserver, makefile, build):
+  print "===> Installing the site for the first time..."
+  run("drush @%s provision-install --debug" % site)
+  run("drush @hostmaster hosting-task @platform_%s verify --debug" % build)
+  time.sleep(5)
+  run("drush @hostmaster hosting-dispatch --debug")
+  time.sleep(5)
+  run("drush @hostmaster hosting-task @%s verify --debug" % site)
+
+# Migrate a site to a new platform
+def migrate_site(site, profile, webserver, dbserver, makefile, build):
+  print "===> Migrating the site to the new platform"
+  run("drush @%s provision-migrate @platform_%s --debug" % (site, build))
+
+# Save the Drush alias to reflect the new platform
+def save_alias(site, profile, webserver, dbserver, makefile, build):
+  print "===> Updating the Drush alias for this site"
+  run("drush provision-save @%s --context_type=site --uri=%s --platform=@platform_%s --web_server=@server_%s --db_server=@server_%s --profile=%s --debug" % (site, site, build, webserver, dbserver, profile))
+
+# Import a site into the frontend, so that Aegir learns the site is now on the new platform
+def import_site(site, profile, webserver, dbserver, makefile, build):
+  print "===> Refreshing the frontend to reflect the site under the new platform"
+  run("drush @hostmaster hosting-import @%s --debug" % site)
+  run("drush @hostmaster hosting-task @platform_%s verify --debug" % build)
+  run("drush @hostmaster hosting-import @%s --debug" % site)
+  run("drush @hostmaster hosting-task @%s verify --debug" % site)
